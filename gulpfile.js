38a69b8292f6f0fc67ea5b5fd59d0e95b3306517
Beshoy Louka <beshoy.louka@gmail.com>
2014-01-24 01:57:14 -0500

    added twbs less files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,17 @@
 var gulp = require('gulp'),
-    nodemon = require('gulp-nodemon');
+    nodemon = require('gulp-nodemon'),
+    recess = require('gulp-recess'),
+    less = require('gulp-less');
+
+
+gulp.task('recess', function () {
+    gulp.src('./public/css/less/bootstrap.less')
+//        .pipe(recess())
+        .pipe(less())
+        .pipe(gulp.dest('./public/css/'));
+});
 
 gulp.task('server', function () {
-  nodemon({ script: 'server.js', options: '-e html,js -i ignored.js' });
+  nodemon({ script: 'server.js', options: '-e html,js -i bootstrap.css' });
 });
+
