b176ccc1121a0bf3af0685485be86c58e4c72768
Beshoy Louka <beshoy.louka@gmail.com>
2014-01-27 14:45:38 -0500

    ready for validations and oauth

--- routes/user.js
+++ routes/user.js
@@ -21,7 +21,7 @@ exports.list = function(req, res){
 exports.create = function(req, res) {
 
  var hashPswd = function(v){
-        var hash = bcrypt.hashSync(v, 8);
+        var hash = bcrypt.hashSync(v, 10);
         return hash;
   };
 
@@ -39,12 +39,8 @@ exports.create = function(req, res) {
 
 exports.destroy = function(req, res) {
   db.User.find({ where: { id: req.param('user_id') } }).success(function(user) {
-    db.Project.find({ where: { id: req.param('project_id') } }).success(function(project) {
-      project.setUser(null).success(function() {
-        project.destroy().success(function() {
-          res.redirect('/users');
-        });
-      });
+    user.destroy().success(function() {
+        res.redirect('/users');
     });
   });
 };
