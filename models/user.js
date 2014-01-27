effa5be1ce7017870dc7faaa3bfe5e8a27b33bd3
Beshoy Louka <beshoy.louka@gmail.com>
2014-01-27 00:16:20 -0500

    trying to hash the password from inside sequelize.js

--- models/user.js
+++ models/user.js
@@ -1,10 +1,19 @@
+var bcrypt = require('bcrypt');
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define('User', {
-    username: DataTypes.STRING
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    title: DataTypes.STRING,
+    password: DataTypes.STRING
   }, {
     associate: function(models) {
       User.hasMany(models.Project);
-    }
+    }/*,
+    instanceMethods: {
+      setPassword: function() {
+        return console.log("HASHED");
+      }
+    }*/
   });
 
   return User;
--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,7 @@
  */
 
 var db = require('../models');
+var bcrypt = require('bcrypt');
 
 exports.list = function(req, res){
   db.User.findAll({
@@ -18,7 +19,51 @@ exports.list = function(req, res){
 
 
 exports.create = function(req, res) {
-  db.User.create({ username: req.param('username') }).success(function() {
-    res.redirect('/users');
+  db.User.create({
+        name: req.param('name'),
+        title: req.param('title'),
+        email: req.param('email'),
+        password: hashPswd(req.param('password'))
+    }).success(function() {
+        res.redirect('/users');
+  });
+/*
+  var user = db.User.build({
+    name: req.param('name'),
+    title: req.param('title'),
+    email: req.param('email'),
+    password: hashPswd(req.param('password'))
+  });
+  user.save().complete(function(err){
+    if (!!err) {
+      console.log('The instance has not been saved:', err);
+    } else {
+      console.log('We have a persisted instance now');
+    }
+  });
+*/
+  /**/
+  function hashPswd(value) {
+    bcrypt.hash(value, 10, function passwordEncrypted(err, encrypted) {
+        // Store hash in your password DB.
+        console.log('insied the hash func encrypted is: ' + encrypted);
+        //TODO: that solution is creating a new entry row in the db with the hashed pswd
+        db.User.create({ password:encrypted }).success(function(){
+            console.log('saved pswd');
+        });
+    });
+  }
+  /**/
+};
+
+exports.destroy = function(req, res) {
+  db.User.find({ where: { id: req.param('user_id') } }).success(function(user) {
+    db.Project.find({ where: { id: req.param('project_id') } }).success(function(project) {
+      project.setUser(null).success(function() {
+        project.destroy().success(function() {
+          res.redirect('/users');
+        });
+      });
+    });
   });
 };
